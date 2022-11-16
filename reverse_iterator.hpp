bc3b44e4a68fe9867ba8501a0820f8073a02a6d5
Anis REMIKI <anremiki@e2r8p20.clusters.42paris.fr>
2022-11-16 19:38:41 +0100

    Vector v1

---
+++ reverse_iterator.hpp
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_iterator.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anremiki <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/16 17:14:48 by anremiki          #+#    #+#             */
+/*   Updated: 2022/11/16 19:30:05 by anremiki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef REVERSE_ITERATOR_HPP
+# define REVERSE_ITERATOR_HPP
+
+#include "iterator_traits.hpp"
+#include <iterator>
+
+template<class Iterator>
+class reverse_iterator
+{
+   public:
+
+         typedef  Iterator iterator_type;
+         typedef typename ft::iterator_traits<Iterator>::value_type value_type;
+         typedef typename ft::iterator_traits<Iterator>::difference_type difference_type;
+         typedef typename ft::iterator_traits<Iterator>::pointer pointer;
+         typedef typename ft::iterator_traits<Iterator>::reference reference;
+         typedef typename ft::iterator_traits<Iterator>::iterator_category iterator_category;
+         
+         reverse_iterator( void ) {};
+         ~reverse_iterator( void ) {};
+
+         reverse_iterator(reverse_iterator & src) {
+            _it = src._it;
+         }
+
+         iterator_type  base( void ) const {
+            return _it;
+         }
+
+         reference  operator*(void) {
+            return *(_it);
+         }
+
+         reverse_iterator  operator+(difference_type n) const{
+            return reverse_iterator(_it - n);
+         }
+
+         reverse_iterator  &operator++(void) {
+            --_it;
+            return *this;
+         }
+
+         reverse_iterator  operator++(int) {
+            reverse_iterator tmp = *this;
+            ++(*this);
+            return tmp;
+         }
+
+         reverse_iterator& operator +=(difference_type n) {
+            _it -= n;
+            return *this;
+         }
+
+         reverse_iterator  operator-(difference_type n) const{
+            return reverse_iterator(_it + n);
+         }
+
+         reverse_iterator  &operator--(void) {
+            ++_it;
+            return *this;
+         }
+
+         reverse_iterator  operator--(int) {
+            reverse_iterator  tmp = *this;
+            --(*this);
+            return tmp;
+         }
+
+         reverse_iterator& operator -=(difference_type n) {
+            _it += n;
+            return *this;
+         }
+
+         pointer  operator->(void) {
+            return &(operator*());
+         }
+
+         reference   operator[](difference_type n) const {
+            return *(_it + n);
+         }
+         
+      private:
+         
+         Iterator _it;
+};
+
+template <class Iterator>
+bool operator==(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() == rhs.base() ? true : false);
+}
+
+template <class Iterator>
+bool operator!=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() != rhs.base() ? true : false);
+}
+
+template <class Iterator>
+bool operator<(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() > rhs.base());
+}
+
+template <class Iterator>
+bool operator<=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() >= rhs.base());
+}
+
+template <class Iterator>
+bool operator>(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() < rhs.base());
+}
+
+template <class Iterator>
+bool operator>=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+   return (lhs.base() <= rhs.base());
+}
+
+template <class Iterator>
+reverse_iterator<Iterator> operator+(typename reverse_iterator<Iterator>::difference_type n, const reverse_iterator<Iterator>& rev_it) {
+   return reverse_iterator<Iterator>(rev_it.base() - n);
+}
+
+template <class Iterator>
+typename reverse_iterator<Iterator>::difference_type operator-( const reverse_iterator<Iterator>& lhs,
+      const reverse_iterator<Iterator>& rhs) {
+   return rhs.base() - lhs.base();
+}
+
+#endif
---
+++ vector.hpp
@@ -0,0 +1,269 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include <functional>
+#if __x86_64__
+#define ARCH 64
+#else
+#define ARCH 32
+#endif 
+
+#include <memory>
+#include <cmath>
+#include "reverse_iterator.hpp"
+
+template< class T, class Alloc = std::allocator<T> >
+class vector
+{
+   public:
+
+      typedef T         value_type;
+      typedef Alloc     allocator_type;
+      typedef T*        iterator;
+      typedef const T*  const_iterator;
+      typedef typename  allocator_type::reference reference;
+      typedef typename  allocator_type::const_reference const_reference;
+      typedef typename  allocator_type::pointer pointer;
+      typedef typename  allocator_type::const_pointer const_pointer;
+      typedef typename  allocator_type::size_type size_type;
+      typedef typename  reverse_iterator<iterator>::reverse_iterator reverse_iterator;
+      typedef typename  reverse_iterator<iterator>::const_reverse_iterator const_reverse_iterator;
+
+
+      /***** Destructor *****/
+      /**********************/
+
+      ~vector() {
+         while (_end != _start)
+            _alloc.destroy(_end--);
+         _alloc.deallocate(_start, capacity());
+      }
+
+      /***** Constructor *****/
+      /***********************/
+
+      /* Empty container constructor (Default constructor) */
+      /* Constructs an empty container, with no elements */
+      explicit vector (const allocator_type& alloc = allocator_type())
+         :  _start(NULL), _end(NULL), _capacity(NULL), _alloc(alloc) {
+         }
+
+      /* Fill constructor */
+      /* Constructs a container with n elements. Each element is a copy of val */
+      explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type())
+         : _start(NULL), _end(NULL), _capacity(NULL), _alloc(alloc) {
+            _start = _alloc.allocate(n);
+            _end = _start;
+            _capacity = _start + n;
+            while (_end != _capacity)
+               _alloc.construct(_end++, val);
+         }
+
+      /* Range constructor */
+      /* Construct a container with as many elements as the range [first, last],
+       * with each element constructed from its corresponding element in that range, in the same order */
+      template <class InputIterator>
+         vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type());
+
+      /* Copy constructor */
+      /* Constructs a container with a copy of each of the elements in x, in the same order */
+      vector( vector const & src )
+         : _start(NULL), _end(NULL), _capacity(src._capacity), _alloc(src._alloc) {
+            _start = _alloc.allocate(src._capacity - src._start);
+            _end = _start;
+            for (pointer ptr = src._start; ptr <= src._end; ptr++)
+               _alloc.construct(_end++, *ptr);
+         }
+
+      vector & operator=( vector const & src );
+
+      /***** Iterator *****/
+      /********************/
+
+      iterator begin(void) {
+         return (_start);
+      }
+
+      const_iterator begin(void) const {
+         return (_start);
+      }
+
+      iterator end(void) {
+         return _end + 1;
+      }
+
+      const_iterator end(void) const {
+         return _end + 1;
+      }
+
+      reverse_iterator rbegin(void) {
+         return reverse_iterator(_end);
+      }
+
+      const_reverse_iterator rbegin(void) const {
+         return reverse_iterator(_end);
+      }
+
+      reverse_iterator rend(void) {
+         return reverse_iterator(_begin);
+      }
+
+      const_reverse_iterator rend(void) const {
+         return reverse_iterator(_begin);
+      }
+      /***** Capacity *****/
+      /********************/
+
+      size_type   size(void) const {
+         return _end - _start;
+      }
+
+      size_type   max_size(void) const {
+         return ((size_type)(pow(2, ARCH)) / sizeof(T) - 1);
+         //return _alloc.max_size();
+      }
+
+      size_type   capacity(void) const {
+         return _capacity - _start;
+      }
+
+      bool        empty(void) const {
+         return (_start == _end ? true : false);
+      }
+
+      void        reserve(size_type n) {
+         if (n > capacity()) {
+            pointer  start;
+            size_type   cap;
+
+            start = _alloc.allocate(n);
+            for (pointer end = start; end != _end; end++)
+               _alloc.construct(end, *_end);
+            clear();
+            cap = capacity();
+            if (cap)
+               _alloc.deallocate(_start, cap);
+            _start = start;
+            _capacity = _start + n;
+         }
+      }
+
+      void        resize(size_type n, value_type val = value_type()) {
+         pointer  ptr = _start + n;
+         if (n < size())
+         {
+            while (ptr < _end)
+               _alloc.destroy(ptr++);
+         }
+         else if (n > size())
+         {
+            if (n > capacity())
+               reserve(n);
+            while (n != size())
+               push_back(val);
+         }
+      }
+
+      /***** Element access *****/
+      /**************************/
+
+      reference operator[](size_type n) {
+         return *(_start + n);
+      }
+
+      const_reference operator[](size_type n) const {
+         return *(_start + n);
+      }
+
+      reference at(size_type n) {
+         return _start[n];
+      }
+
+      const_reference at(size_type n) const {
+         return _start[n];
+      }
+
+      reference front(void) {
+         return *(_start);
+      }
+
+      const_reference front(void) const {
+         return *(_start);
+      }
+
+      reference back(void) {
+         return *(_end);
+      }
+      const_reference back(void) const {
+         return *(_end);
+      }
+
+      /***** Modifiers *****/
+      /*********************/
+
+      void     assign(size_type n, const value_type &val) {
+         if (n <= capacity()) {
+            for (pointer ptr = _start; ptr < _end; ptr++) {
+               _alloc.destroy(ptr);
+               _alloc.construct(ptr, val);
+            }
+         }
+         else {
+            for (pointer ptr = _start; ptr < _capacity; ptr++)
+               _alloc.destroy(ptr);
+            _alloc.deallocate(_start, capacity());
+            _start = _alloc.allocate(n);
+            for (_end = _start; _end < _start + n; _end++)
+               _alloc.construct(_end, val);
+            _capacity = _start + n;
+         }
+      }
+
+      void     push_back(const value_type& val) {
+         if (size() + 1 < capacity())
+            _alloc.construct(++_end, val);
+      }
+
+      void     pop_back(void) {
+         _alloc.destroy(_end--);
+      }
+
+      void     swap(vector& x) {
+         pointer startcpy;
+         pointer endcpy;
+         pointer capacitycpy;
+
+         startcpy = _start;
+         endcpy = _end;
+         capacitycpy = _capacity;
+
+         _start = x._start;
+         _end = x._end;
+         _capacity = x._capacity;
+
+         x._start = startcpy;
+         x._end = endcpy;
+         x._capacity = capacitycpy;
+      }
+
+      void     clear(void) {
+         while (_end != _start)
+            _alloc.destroy(_end--);
+      }
+
+      /***** Allocator *****/
+      /*********************/
+
+      allocator_type get_allocator() const {
+         return _alloc;
+      }
+
+   private:
+
+      pointer        _start;
+      pointer        _end;
+      pointer        _capacity;
+      allocator_type _alloc;
+};
+
+#endif
